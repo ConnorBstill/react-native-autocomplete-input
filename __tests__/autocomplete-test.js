5632f49d37626f83d4f244011f169693ea8a6564
Laurence Bortfeld <cheese@byteburgers.com>
2019-11-13 18:26:33 +0100

    Add test for default list properties

--- __tests__/autocomplete-test.js
+++ __tests__/autocomplete-test.js
@@ -12,17 +12,17 @@ const ITEMS = [
   'Revenge of the Sith'
 ];
 
-const keyExtractor = (item, index) => `key-${index}`;
+const keyExtractor = (_, index) => `key-${index}`;
 
 describe('<AutocompleteInput />', () => {
-  it('Should hide suggestion list on initial render', () => {
+  it('should hide suggestion list on initial render', () => {
     const r = renderer.create(<Autocomplete data={[]} />);
     const autocomplete = r.root;
 
     expect(autocomplete.findAllByType(FlatList)).toHaveLength(0);
   });
 
-  it('Should show suggestion list when data gets updated with length > 0', () => {
+  it('should show suggestion list when data gets updated with length > 0', () => {
     const testRenderer = renderer.create(<Autocomplete data={[]} />);
     const autocomplete = testRenderer.root;
 
@@ -37,7 +37,7 @@ describe('<AutocompleteInput />', () => {
     expect(texts).toHaveLength(ITEMS.length);
   });
 
-  it('Should hide suggestion list when data gets updates with length < 1', () => {
+  it('should hide suggestion list when data gets updates with length < 1', () => {
     const props = { data: ITEMS, keyExtractor };
     const testRenderer = renderer.create(<Autocomplete {...props} />);
     const autocomplete = testRenderer.root;
@@ -68,5 +68,16 @@ describe('<AutocompleteInput />', () => {
     const textInput = autocomplete.findByType(TextInput);
 
     expect(textInput.props).toEqual(expect.objectContaining(props));
+  });
+
+  describe('properties', () => {
+    describe('Given no custom list properties are supplied', () => {
+      it('should set default properties', () => {
+        const { root: autocomplete } = renderer.create(<Autocomplete data={ITEMS} />);
+        const list = autocomplete.findByType(FlatList);
+
+        expect(list.props).toMatchSnapshot();
+      });
+    });
   });
 });
